ea18d2b8a17fe20a23c19177268bacdb37c3cca5
Lee Jae Hyun <myleje@gmail.com>
2021-08-01 17:50:59 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -5,9 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext ctx)
     {
-        Vector2 dir = value.Get<Vector2>();
+        Vector2 dir = ctx.ReadValue<Vector2>();
         Debug.Log($"Move = ({dir.x},{dir.y})");
     }
 }
